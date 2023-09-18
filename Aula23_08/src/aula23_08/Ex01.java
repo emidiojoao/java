5e8dc6d462779427c4e3bbcaf44310c04f61f574
emidiojoao <143660761+emidiojoao@users.noreply.github.com>
2023-09-17 21:22:57 -0300

    Update Ex01.java

--- Aula23_08/src/aula23_08/Ex01.java
+++ Aula23_08/src/aula23_08/Ex01.java
@@ -3,11 +3,6 @@ package aula23_08;
 import java.util.Scanner;
 
 public class Ex01 {
-
-    /* 
-    Desenvolva um algoritmo que receba o valor da base e da altura de um triângulo e mostre sua área
-    A área do triangulo retângulo é calculada por meio da formula area ==(base * altura) 2
-     */
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
