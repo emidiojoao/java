e2a1905a4d08bfbcecd3fdaa90dfb938459b7b82
emidiojoao <143660761+emidiojoao@users.noreply.github.com>
2023-11-29 21:06:39 -0300

    upload atividades matrizes

---
+++ aula10_matrizes/Exemplo3.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula10_matrizes;
+
+/**
+ *
+ * @author joao_c_emidio
+ */
+public class Exemplo3 {
+    
+    public static void main(String[] args) {
+        int[][] matriz = {{1, 2, 3, 4}, {2, 1, 4, 3}, {3, 4, 1, 2}, {4, 3, 2, 1}};
+        boolean simetrica = true;
+        
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] != matriz[j][i]) {
+                    simetrica = false;
+                    break;
+                }
+            }
+        }
+        
+        if (simetrica) {
+            System.out.println("A matriz é simétrica.");
+        } else {
+            System.out.println("A matriz não é simétrica.");
+        }
+    }
+}
+
+}
